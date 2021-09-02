994721ee1e39e6748f266a284956b18856317e31
SebastVR <sebastianvr92@hotmail.com>
2021-09-01 19:10:33 -0500

    Added https://www.hackerrank.com/challenges/python-division/problem #1

---
+++ exercises/division.py
@@ -0,0 +1,13 @@
+""" Task
+The provided code stub reads two integers, a and b, from STDIN.
+
+Add logic to print two lines. The first line should contain the result of integer division,  // . 
+The second line should contain the result of float division,  / .
+
+No rounding or formatting is necessary. """
+
+a = int(input("Write first number: "))
+b = int(input("Write first second: "))
+
+print(a // b)  # Division integer
+print(a / b)  # Division float
