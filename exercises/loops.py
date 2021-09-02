c8dcca307766b6053fd58b25b8026612760336d3
SebastVR <sebastianvr92@hotmail.com>
2021-09-01 20:49:35 -0500

    Added https://www.hackerrank.com/challenges/python-loops/problem #1

---
+++ exercises/loops.py
@@ -0,0 +1,11 @@
+""" Task
+The provided code stub reads and integer, n, from STDIN. For all non-negative integers i < n,
+print i**2 . """
+
+i = int(input("Write) a number: "))
+
+for i in range(i):
+    print(i ** 2)
+
+# ----- List Comprenhensions -----
+l = [i ** 2 for i in range(i)]
